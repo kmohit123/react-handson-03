372edb79d461baf068f47325da5bfa12be8d9bae
kmohit123 <mkumar8804978@gail.com>
2023-09-06 02:07:30 +0500

    change

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
 import React from 'react';
 import ClassCompo from './Component/ClassCompo';
 import './App.css';
-// import DisplayFune from './Component/DisplayFune';
 function App() {
   return (
     <>
-      {/* <h1 className='hading'>EMPLOYEE FEEDBACK FORM</h1> */}
       <ClassCompo/>
-      {/* <DisplayFune/> */}
     </>
   );
 }
--- src/Component/DisplayFune.js
+++ src/Component/DisplayFune.js
@@ -1,33 +1,36 @@
 // import { render } from "@testing-library/react";
 import React from 'react'
-import { Component } from "react";
-class DisplayFune extends Component{
-    constructor(props){
-        super(props);
-        console.log(this.props.value)
-    }
-    render() {
-        return(
-            <>
-            
-             <h1 className='hading'>EMPLOYEE FEEDBACK DATA</h1>
-             <div className='allop'>
-             {this.props.value.map((item, index) => {
-                
-                return (
+import '../App.css'
+import { Component } from 'react'
+class DisplayFune extends Component {
+  constructor(props) {
+    super(props)
+    console.log(this.props.value)
+  }
+  render() {
+    return (
+      <>
+        <div className="cantener">
+            <h1 className="hading">EMPLOYEE FEEDBACK DATA</h1>
+          <div className="allop">
+            {this.props.value.map((item, index) => {
+              return (
                 <>
-                    <span>
-                    Name :{item.name} || Dept : {item.dept} || Rating : {item.rating}
-                    </span>
-                    {/* <button onClick={this.props.toggleFune}>Go Back</button> */}
+                  <span>
+                    Name :{item.name} || Dept : {item.dept} || Rating :{' '}
+                    {item.rating}
+                  </span>
+                  {/* <button onClick={this.props.toggleFune}>Go Back</button> */}
                 </>
-                )
-             })}
-             </div>
-               <button onClick={this.props.toggleFune}>Go Back</button>
-            </>
-        )
-    }
+              )
+            })}
+          </div>
+          <center><button onClick={this.props.toggleFune} className='pp'>Go Back</button></center>
+        </div>
+        
+      </>
+    )
+  }
 }
-   
-export default DisplayFune;
+
+export default DisplayFune
